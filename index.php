1bc4fdffab8d3ff98315620c1cf6e3d89c189200
Nick Breen <nick@foobar.net.nz>
2016-04-23 16:52:41 +1200

    added nicer test index.php, stripped unwanted vary header values, fixes #4

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+if ('https' == $_SERVER['HTTP_X_FORWARDED_PROTO'])
+  $_SERVER['HTTPS'] = 'on';
+
+// setcookie("HTTPS", $_SERVER['HTTPS']);
+?>
+<a href="/">/</a>
+<table>
+<?php
 foreach ($_SERVER as $h => $v)
-  if (0 === strpos($h, 'HTTP_'))
-    echo "$h\t$v\n";
+  if (0 === strpos($h, 'HTTP'))
+    echo "<tr><td>$h<td>$v\n";
+?>
+</table>
