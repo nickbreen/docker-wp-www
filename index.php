d8bcd94ad9625f4a914ac6afce87cd0b2f57c5fc
Nick Breen <nick@foobar.net.nz>
2016-04-23 23:43:08 +1200

    added stale cache control extensions, added expiry header to index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ if ('https' == $_SERVER['HTTP_X_FORWARDED_PROTO'])
   $_SERVER['HTTPS'] = 'on';
 
 header('Cache-Control: max-age=5, s-maxage=13');
+header("Expires: " . gmdate("D, d M Y H:i:s e", time() + 30) );
 ?>
 <a href="/">/</a>
 <table>
