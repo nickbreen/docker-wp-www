9f06e08deb4b836494c7348d06a9de01a6a64401
Nick Breen <nick@foobar.net.nz>
2016-10-12 21:19:12 +1300

    testing fixing cache issues

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ if ('https' == @$_SERVER['HTTP_X_FORWARDED_PROTO']) {
 
 header('Content-Type: text/plain');
 header('Cache-Control: max-age=15, s-maxage=30');
-//header("Expires: " . gmdate("D, d M Y H:i:s e", time() + 30));
+header("Location: /test.txt");
 ?>
 
 <?php
@@ -16,7 +16,12 @@ $n = array_reduce(array_keys($_SERVER), function ($carry, $item) {
 }, 0);
 
 foreach ($_SERVER as $h => $v) {
-    printf("%-${n}s\t%s".PHP_EOL, $h, $v);
+    header("X-${h}: ${v}");
+    // printf("%-${n}s\t%s".PHP_EOL, $h, $v);
 }
 
-echo "Pass!";
+
+echo $_SERVER['DOCUMENT_ROOT'].PHP_EOL;
+echo $_SERVER['REQUEST_URI'].PHP_EOL;
+
+echo (substr($_SERVER['REQUEST_URI'], 0, strlen($_SERVER['DOCUMENT_ROOT'])) === $_SERVER['DOCUMENT_ROOT'] ? "Fail" : "Pass!").PHP_EOL;
