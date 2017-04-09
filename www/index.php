a906ba68358c076fbfd579dd0e3931daced8068c
Nick Breen <nick@foobar.net.nz>
2017-04-09 17:52:31 +1200

    actually proper no-cache handling; but still no socache save

--- www/index.php
+++ www/index.php
@@ -6,22 +6,11 @@ if ('https' == @$_SERVER['HTTP_X_FORWARDED_PROTO']) {
 
 header('Content-Type: text/plain');
 header('Cache-Control: max-age=15, s-maxage=30');
-// header("Location: /test.txt");
-?>
 
-<?php
-
-$n = array_reduce(array_keys($_SERVER), function ($carry, $item) {
-    return max($carry, strlen($item));
-}, 0);
-
-foreach ($_SERVER as $h => $v) {
-    // header("X-${h}: ${v}");
-    // printf("%-${n}s\t%s".PHP_EOL, $h, $v);
+foreach (['REQUEST_URI','PATH_INFO'] as $i) {
+    if (!isset($_SERVER[$i])) continue;
+    echo $i.": ".$_SERVER[$i].PHP_EOL;
+    user_error($i.": ".$_SERVER[$i]);
 }
-
-
-echo $_SERVER['DOCUMENT_ROOT'].PHP_EOL;
-echo $_SERVER['REQUEST_URI'].PHP_EOL;
 
 echo (substr($_SERVER['REQUEST_URI'], 0, strlen($_SERVER['DOCUMENT_ROOT'])) === $_SERVER['DOCUMENT_ROOT'] ? "Fail" : "Pass!").PHP_EOL;
